c2b529bc1a3bc802962b0b8562078bc5b7e0bc86
Nahuel Benerozzo <axelbenerozzo12@gmail.com>
2020-08-11 16:42:06 -0300

    install material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
-olaaaaa <h1>f,lbl,t</h1>
+      <Button variant="text" color="secondary">
+        olaa
+      </Button>
     </div>
   );
 }
