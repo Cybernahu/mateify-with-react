73e535a4ef418452ed5df9ef5a5e078c2dc0419c
Nahuel Benerozzo <axelbenerozzo12@gmail.com>
2020-08-14 16:55:37 -0300

    comienzo a crear el login page

--- src/components/loginMenu.jsx
+++ src/components/loginMenu.jsx
@@ -2,20 +2,60 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import './../style/Login.css'
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import "./../style/Login.css";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Box from "@material-ui/core/Box";
+import Button from '@material-ui/core/Button';
 
 const LoginMenu = () => {
   return (
     <div>
-      <AppBar position="static" className="NavBar">
-        <Toolbar variant="dense" >
-          <Typography variant="h6" color="inherit" className="typeOfLetter">
-            Mateify
+      <header>
+        <AppBar position="static" className="NavBar">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit" className="typeOfLetter">
+              Mateify
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </header>
+      <div>
+        <Paper elevation={2} outlined className="loginContainer">
+          <LockOutlinedIcon
+            fontSize="large"
+            className="iconLoginBg"
+          ></LockOutlinedIcon>
+
+          <Typography variant="h4" className="displayBlock">
+            INGRESAR
           </Typography>
-        </Toolbar>
-      </AppBar>
+
+          <form className="positionContent displayBlock">
+            <Box mb="44px">
+              <TextField
+                variant="outlined"
+                label="Correo electronico"
+                className="loginInput"
+              />
+            </Box>
+            <Box mb="44px">
+              <TextField
+                variant="outlined"
+                label="Contraseña"
+                className="loginInput"
+                mb="20px"
+              />
+            </Box>
+            <Box>
+              <Button variant="contained" color="secondary">
+                Comenzar a crear playlist
+              </Button>
+            </Box>
+          </form>
+        </Paper>
+      </div>
     </div>
   );
 };
