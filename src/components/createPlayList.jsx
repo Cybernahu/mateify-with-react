2cc079df1ee0c2ab383767504a5f63e7891eb53d
Nahuel Benerozzo <axelbenerozzo12@gmail.com>
2020-08-17 18:04:14 -0300

    finish base of login page

---
+++ src/components/createPlayList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CreatePlayList = () =>{
+    return(
+        <div>
+            hola
+        </div>
+    )
+}
+export default CreatePlayList;
--- src/components/loginMenu.jsx
+++ src/components/loginMenu.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
 import "./../style/Login.css";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Box from "@material-ui/core/Box";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
+import DataUser from './../dataBase/userData.json';
 
-const LoginMenu = () => {
+const LoginMenu = (props, e) => {
+  const [inputUser, setInputUser] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+
+  const inputContentChange = (e) => setInputUser(e.target.value);
+  const inputPasswordChange = (e) => setInputPassword(e.target.value);
+
+  const chequeaUsuariosConFilter = (element) => {
+    if (element.email === inputUser && element.password === inputPassword) {
+      return true;
+    }
+};
+  const usuarios = DataUser.filter(chequeaUsuariosConFilter);
+  console.log(usuarios);
+
   return (
     <div>
       <header>
@@ -27,10 +43,11 @@ const LoginMenu = () => {
             fontSize="large"
             className="iconLoginBg"
           ></LockOutlinedIcon>
-
-          <Typography variant="h4" className="displayBlock">
-            INGRESAR
-          </Typography>
+          <Box mb="30px">
+            <Typography variant="h5" className="displayBlock">
+              INGRESAR
+            </Typography>
+          </Box>
 
           <form className="positionContent displayBlock">
             <Box mb="44px">
@@ -38,22 +55,45 @@ const LoginMenu = () => {
                 variant="outlined"
                 label="Correo electronico"
                 className="loginInput"
+                value={inputUser}
+                onChange={inputContentChange}
               />
             </Box>
             <Box mb="44px">
               <TextField
+                type="password"
                 variant="outlined"
                 label="Contraseña"
                 className="loginInput"
+                value={inputPassword}
+                onChange={inputPasswordChange}
                 mb="20px"
               />
             </Box>
             <Box>
-              <Button variant="contained" color="secondary">
+              <Button
+                variant="contained"
+                color="secondary"
+                className="createPlatList"
+              >
                 Comenzar a crear playlist
               </Button>
             </Box>
           </form>
+          <Box mt="20px" pb="20px" className="forgotPassword">
+            <Button className="forgotPasswordButton">
+              ¿HAS OLVIDADO TU CONTRASEÑA?
+            </Button>
+          </Box>
+
+          <Box mb="30px">
+            <Link href="#">¿NO TIENES CUENTA?</Link>
+          </Box>
+          <Box>
+            <Button color="secondary" variant="outlined">
+              REGISTRATE
+            </Button>
+          </Box>
         </Paper>
       </div>
     </div>
